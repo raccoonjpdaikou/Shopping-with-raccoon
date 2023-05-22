c22de9d683ad988af2d86c3f7d56ff82d73dc63b
Cecilia <Cecilia222202@gmail.com>
2023-05-23 04:38:40 +0800

    no message

--- frontend/src/services/gas.service.js
+++ frontend/src/services/gas.service.js
@@ -9,19 +9,20 @@ class GasService {
   }
   /*跟團 */
   orderGet2(username) {
-    return axios.post(API_URL + "/order1", {
+    return axios.post(API_URL + "/order2", {
       username,
     });
   }
 
   /*現地*/
   orderGet3(username) {
-    return axios.post(API_URL + "/order1", {
+    return axios.post(API_URL + "/order3", {
       username,
     });
   }
 }
 
-export default new GasService();
 /* https://cors-anywhere.herokuapp.com/ */
 /* https://crossorigin.me/ */
+
+export default new GasService();
