fabb633a866128495f097dfed768eea58a476622
Cecilia <Cecilia222202@gmail.com>
2023-06-01 05:27:16 +0800

    no message

--- frontend/src/pages/Home/Comments.js
+++ frontend/src/pages/Home/Comments.js
@@ -56,6 +56,8 @@ const Comments = () => {
   useEffect(() => {
     modal.current = new Modal("#Modal");
     getReply();
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
